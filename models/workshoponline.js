924ed30c91b70f0e29c5b8968ae9d5892e26ab16
mister-ard97 <rezardiansyah1997@gmail.com>
2020-07-27 08:53:41 +0700

    update_model

--- models/workshoponline.js
+++ models/workshoponline.js
@@ -1,18 +1,23 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const workshopOnline = sequelize.define('workshopOnline', {
-    program_name: DataTypes.STRING,
-    thumbnail_video: DataTypes.STRING,
-    slug: DataTypes.STRING,
-    module_name: DataTypes.STRING,
-    session_name: DataTypes.STRING,
-    topic_name: DataTypes.STRING,
-    video_description: DataTypes.STRING,
-    isDeleted: DataTypes.INTEGER,
-    videoSproutId: DataTypes.STRING,
-    securityTokenSprout: DataTypes.STRING
-  }, {});
-  workshopOnline.associate = function(models) {
+  const workshopOnline = sequelize.define(
+    "workshopOnline",
+    {
+      program_name: DataTypes.STRING,
+      thumbnail_video: DataTypes.STRING,
+      slug: DataTypes.STRING,
+      module_name: DataTypes.STRING,
+      session_name: DataTypes.STRING,
+      topic_name: DataTypes.STRING,
+      video_description: DataTypes.STRING,
+      isDeleted: DataTypes.INTEGER,
+      videoSproutId: DataTypes.STRING,
+      securityTokenSprout: DataTypes.STRING,
+      durationVideo: DataTypes.STRING,
+    },
+    {}
+  );
+  workshopOnline.associate = function (models) {
     // associations can be defined here
   };
   return workshopOnline;
